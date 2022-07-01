3ebdd11263df9eb631151eba5d4e14ea1d6ef4d0
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 10:29:33 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function isare(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "+" || str[i] == "-" || str[i] == "*") {
+      count++;
+    }
+  }
+  return count;
+}
+let isareSayi = isare("-1+2*3+a");
+console.log(isareSayi);
