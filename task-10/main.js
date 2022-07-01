3f1315b6423d28c3a530a6556fd89d5da9f7472d
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 12:26:58 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
+let string = "I am programming on Python.";
+const space = (string) => {
+  let newArray = [];
+  let num = 0;
+  let isSpace = false;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == " ") {
+      newArray[num] = i;
+      num++;
+      isSpace = true;
+    }
+  }
+  if (isSpace) {
+    console.log(`${newArray[0]} ${newArray[newArray.length - 1]}`);
+  } else {
+    console.log(-1);
+  }
+};
+space(string);
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
