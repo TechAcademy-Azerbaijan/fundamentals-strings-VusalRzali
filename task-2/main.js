b8b0612d927de78ffe69c8eba0953678f8779e88
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 10:16:44 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,22 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let n = parseInt(result.input)
+    
+    let last = n % 10;
+    let first;
+    while (n > 10) {
+      n = parseInt(n / 10);
+    }
+    first = n;
+    if (first > last) {
+        console.log(first);
+    }
+    if (first < last) {
+        console.log(last);
+    }
+    if (first == last) {
+        console.log("=");
+    }
+
+})
