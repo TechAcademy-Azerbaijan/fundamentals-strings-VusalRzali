15db01057763d1326b8dbe94fc3e01edf2dbe36e
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 12:20:20 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,6 +1,10 @@
-// Use these variables in your code
-
-let s = "This is a cat sitting on a table"
-let c = "t"
-
-// Write code here. 
+let str = "This is a cat sitting on a table";
+const boyukHerf = (str) => {
+  let strArray = str.split(" ");
+  let result = "";
+  for (let i = 0; i < strArray.length; i++) {
+    result = result + strArray[i][0].toUpperCase() + strArray[i].slice(1) + " ";
+  }
+  console.log(result);
+};
+boyukHerf(str);
