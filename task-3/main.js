7daf46fcda9276e1a647e9c5a8b461d9cc97545d
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 10:18:25 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,24 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
+    let n = parseInt(result.input)
+    let iki = 0,
+      bes = 0;
+    while (n > 0) {
+      rem = n % 10;
+      n = parseInt(n / 10)
+      if (rem == 2) {
+        iki++
+      } else if (rem == 5) {
+        bes++
+      }
+    }
+    if (iki > bes) {
+      console.log(bes);
+    } else if (bes > iki) {
+      console.log(iki);
+    } else {
+     console.log("=")
+    }
   
-    // Write code here
-  
-});
+})
