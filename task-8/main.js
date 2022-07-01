54cb051c1af598e4cf2c918ff079b0705eb03256
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 12:25:52 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = "introduction to algorithms";
+const boyukHerf = (str) => {
+  let strArray = str.split(" ");
+  let result = "";
+  for (let i = 0; i < strArray.length; i++) {
+    result = result + strArray[i][0].toUpperCase() + strArray[i].slice(1) + " ";
+  }
+  console.log(result);
+};
+boyukHerf(str);
