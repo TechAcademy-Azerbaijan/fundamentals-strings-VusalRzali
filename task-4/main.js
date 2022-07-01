180a04c797edcc0a6ade7426efdd21bab17e906f
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 10:27:53 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,14 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function cumle(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "." || str[i] == "!") {
+      if (str[i + 1] == ".") {
+        continue;
+      }
+      count++;
+    }
+  }
+  return count;
+}
+let cumleSayi = cumle("Hello world! I learn to code.");
+console.log(cumleSayi);
